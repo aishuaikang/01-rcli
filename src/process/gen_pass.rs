a0abc257e532967ac58aa0eb87656a44efc81f39
艾帅康 <2506377990@qq.com>
2024-11-06 11:39:17 +0800

    新增

--- src/process/gen_pass.rs
+++ src/process/gen_pass.rs
@@ -1,4 +1,10 @@
 use rand::seq::SliceRandom;
+use zxcvbn::zxcvbn;
+
+static LOWERCASE: &[u8] = b"abcdefghijkmnopqrstuvwxyz";
+static UPPERCASE: &[u8] = b"ABCDEFGHJKLMNPQRSTUVWXYZ";
+static NUMBER: &[u8] = b"123456789";
+static SYMBOL: &[u8] = b"!@#$%^&*()-_=+";
 
 pub fn process_gen_pass(
     length: u8,
@@ -7,45 +13,43 @@ pub fn process_gen_pass(
     number: bool,
     symbol: bool,
 ) -> anyhow::Result<()> {
-    println!("{lowercase} - {uppercase}");
     let mut rng = rand::thread_rng();
     let mut password = Vec::new();
     let mut chars = Vec::new();
 
     if lowercase {
-        chars.extend_from_slice(b"abcdefghijkmnopqrstuvwxyz");
-        let c = chars
+        chars.extend_from_slice(LOWERCASE);
+        let c = LOWERCASE
             .choose(&mut rng)
             .expect("在这个上下文中，字符不会为空。");
         password.push(*c);
     }
 
     if uppercase {
-        chars.extend_from_slice(b"ABCDEFGHJKLMNPQRSTUVWXYZ");
-        let c = chars
+        chars.extend_from_slice(UPPERCASE);
+        let c = UPPERCASE
             .choose(&mut rng)
             .expect("在这个上下文中，字符不会为空。");
         password.push(*c);
     }
 
     if number {
-        chars.extend_from_slice(b"123456789");
-        let c = chars
+        chars.extend_from_slice(NUMBER);
+        let c = NUMBER
             .choose(&mut rng)
             .expect("在这个上下文中，字符不会为空。");
         password.push(*c);
     }
 
     if symbol {
-        chars.extend_from_slice(b"!@#$%^&*()-_=+");
-        let c = chars
+        chars.extend_from_slice(SYMBOL);
+        let c = SYMBOL
             .choose(&mut rng)
             .expect("在这个上下文中，字符不会为空。");
         password.push(*c);
     }
 
     for _ in 0..(length - password.len() as u8) {
-        // let idx = rng.gen_range(0..chars.len());
         let c = chars
             .choose(&mut rng)
             .expect("在这个上下文中，字符不会为空。");
@@ -54,7 +58,11 @@ pub fn process_gen_pass(
 
     password.shuffle(&mut rng);
 
-    println!("{}", String::from_utf8(password).unwrap());
+    let password = String::from_utf8(password)?;
+    println!("{}", password);
+
+    let password_strength = zxcvbn(&password, &[]);
+    eprintln!("{:?}", Into::<u8>::into(password_strength.score()));
 
     Ok(())
 }
